68a8eedb1e95d4b1e18a435dba4fe1c4f470b0fa
mccmar <125001432+mccmar@users.noreply.github.com>
2024-11-06 14:22:27 +0100

    Add files via upload

---
+++ .github/workflows/export-metrics-test4.ps1
@@ -0,0 +1,28 @@
+﻿# Configura il tuo token GitHub
+$token = "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"
+$headers = @{
+    Authorization = "token $token"
+    Accept = "application/vnd.github.v3+json"
+}
+
+# Fai una richiesta API per ottenere i repository
+$response = Invoke-RestMethod -Uri "https://api.github.com/repos/mccmar/testpowerbipub/actions/runs" -Headers $headers
+
+$repos = $response | ForEach-Object {
+    $_.workflow_runs | ForEach-Object {
+        [PSCustomObject]@{
+            id = $_.id
+            name = $_.name
+            status = $_.status
+            conclusion = $_.conclusion
+            actor = $_.actor.login
+            repository = $_.repository.name
+        }
+    }
+}
+
+
+# Salva i dati in un file CSV
+$repos | Export-Csv -Path "github_repos4.csv" -NoTypeInformation
+
+Write-Output "Dati esportati con successo in github_repos4.csv"
